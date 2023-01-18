339c2d0cf6434590a43eda8f059eaaa6150ec687
Sergey <yuzvenko.sergey@gmail.com>
2023-01-18 22:03:35 +0200

    add section Blocks

--- SpinText/Views/Shared/_DropDownBlock.cshtml.js
+++ SpinText/Views/Shared/_DropDownBlock.cshtml.js
@@ -1,6 +1,25 @@
 // drop-down-block
 jQuery('.dc-drop-down-block').dcTpl(function ($, Export) {
-   var $self = $(this);
+    var $self = $(this);
+    var deg = 0;
+    $self.on('click', '.dcj-toggle-btn', function (e) {
+        e.preventDefault();
+        $self.find(".dc-drop-down-block__content").stop().animate({ "height": "toggle" });
+        if (deg === 0) {
+            deg -= 180;
+            $self.find(".dc-drop-down-block__arrow").css({
+                transform: 'rotate(' + deg + 'deg)',
+                transition: "transform 1s"
+            })
+        } else {
+            deg = 0;
+            $self.find(".dc-drop-down-block__arrow").css({
+                transform: 'rotate(' + deg + 'deg)'
+            })
+        }
+    })
+
+
 });
 // /drop-down-block
 //--------------------------------------------
