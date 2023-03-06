06eaf623839e7f286fffbdef2acc22a19731c583
evg2576 <24evgeniy03@gmail.com>
2023-03-06 22:27:05 +0300

    -

--- SpinText/FaqParser/FaqParser.cs
+++ SpinText/FaqParser/FaqParser.cs
@@ -1,6 +1,6 @@
 ﻿using AngleSharp;
+using AngleSharp.Dom;
 using DobriyCoder.Core.Errors.Old;
-using System.Security.Cryptography.X509Certificates;
 
 namespace SpinText.FaqParser;
 
@@ -9,13 +9,10 @@ public class FaqParser : IFaqParser
     public const string FAQ_NAME = "faq";
     public const string ITEM_NAME = "item";
     public const string QUESTION_NAME = "question";
-    public const string H3_NAME = "h3";
     public const string ANSWER_NAME = "answer";
-    public const string P_NAME = "p";
 
     public FaqParserResult Parse(string text)
     {
-
         var result = new FaqParserResult();
         result.Faq = new List<Dictionary<string, string>>();
         result.Content = text;
@@ -26,30 +23,50 @@ public class FaqParser : IFaqParser
             var config = Configuration.Default;
             using var context = BrowsingContext.New(config);
             using var doc = context.OpenAsync(req => req.Content(text)).Result;
-
             Dictionary<string, string> res;
+
             var faqs = doc.GetElementsByTagName(FAQ_NAME);
+            if (faqs.Count() == 0) throw new Exception($"Тег {FAQ_NAME} отсутствует в документе");
+
             foreach (var faq in faqs)
             {
-                res = new Dictionary<string?, string?>();
-                foreach (var item in faq.GetElementsByTagName(ITEM_NAME))
-                    res.Add(item?
-                        .GetElementsByTagName(QUESTION_NAME)?
-                        .FirstOrDefault()?
-                        .GetElementsByTagName(H3_NAME)?
-                        .FirstOrDefault()?
-                        .InnerHtml
-                        , item?
-                        .GetElementsByTagName(ANSWER_NAME)?
-                        .FirstOrDefault()?
-                        .GetElementsByTagName(P_NAME)?
-                        .FirstOrDefault()?
-                        .InnerHtml);
+                res = new Dictionary<string, string>();
+
+                var items = faq.GetElementsByTagName(ITEM_NAME);
+                if (items.Count() == 0) 
+                    throw new Exception($"Тег {ITEM_NAME} отсутствует в документе");
+
+                IHtmlCollection<IElement> questions, answers;
+                foreach (var item in items)
+                {
+                    questions = item.GetElementsByTagName(QUESTION_NAME);
+                    answers = item.GetElementsByTagName(ANSWER_NAME);
+
+                    if (questions.Count() == 0)
+                        throw new Exception($"Тег {QUESTION_NAME} отсутствует в документе");
+                    else if (answers.Count() == 0)
+                        throw new Exception($"Тег {ANSWER_NAME} отсутствует в документе");
+
+                    if(res.ContainsKey(questions
+                        .First()
+                        .InnerHtml))
+                        res[questions
+                        .First()
+                        .InnerHtml] = answers
+                        .First()
+                        .InnerHtml;
+                    else res.Add(questions
+                            .First()
+                            .InnerHtml
+                            , answers
+                            .First()
+                            .InnerHtml);
+                }
                 result.Faq.Add(res);
             }
             foreach (var element in doc.QuerySelectorAll(FAQ_NAME))
-                element.Remove();
-            result.ContentWithoutFaq = doc.ToHtml();
+                element.Replace(element.ChildNodes.ToArray());
+            result.ContentWithoutFaq = doc?.Body?.InnerHtml;
         }
         catch (Exception ex)
         {
--- SpinText/FaqParser/FaqParserResult.cs
+++ SpinText/FaqParser/FaqParserResult.cs
@@ -4,8 +4,8 @@ namespace SpinText.FaqParser;
 
 public class FaqParserResult
 {
-    public string Content { get; set; }
-    public string ContentWithoutFaq { get; set; }
-    public List<Dictionary<string, string>> Faq { get; set; }
-    public IErrors Errors { get; set; }
+    public string? Content { get; set; }
+    public string? ContentWithoutFaq { get; set; }
+    public List<Dictionary<string, string>>? Faq { get; set; }
+    public IErrors? Errors { get; set; }
 }
