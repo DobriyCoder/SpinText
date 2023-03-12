63c1a717700d48738d31e1d0aa36281ed8891eb2
evg2576 <24evgeniy03@gmail.com>
2023-03-12 15:21:43 +0300

    add regex parser

--- SpinText/FaqParser/FaqParser.cs
+++ SpinText/FaqParser/FaqParser.cs
@@ -1,6 +1,7 @@
 ﻿using AngleSharp;
 using AngleSharp.Dom;
 using DobriyCoder.Core.Errors.Old;
+using System.Text.RegularExpressions;
 
 namespace SpinText.FaqParser;
 
@@ -11,6 +12,24 @@ public class FaqParser : IFaqParser
     public const string QUESTION_NAME = "question";
     public const string ANSWER_NAME = "answer";
 
+    public FaqParserResult RegexParse(string text)
+    {
+        var res = new FaqParserResult();
+        res.Content = text;
+
+        string input = "This written in <b>bold fonts</b>. This is simple font <b>again bold fonts</b>";
+        string regex = @" <b>\s*(.+?)\s*</b>";
+        var matches = Regex.Matches(input, regex);
+        if (matches.Count > 0)
+        {
+            foreach (Match m in matches)
+            {
+                res.ContentWithoutFaq += m.Groups[1].Value + "\n\n";
+            }
+        }
+        return res;
+    }
+
     public FaqParserResult Parse(string text)
     {
         var result = new FaqParserResult();
@@ -18,61 +37,60 @@ public class FaqParser : IFaqParser
         result.Content = text;
         result.Errors = new Errors();
 
-        try
-        {
-            var config = Configuration.Default;
-            using var context = BrowsingContext.New(config);
-            using var doc = context.OpenAsync(req => req.Content(text)).Result;
-            Dictionary<string, string> res;
+        //try
+        //{
+        //    var config = Configuration.Default;
+        //    using var context = BrowsingContext.New(config);
+        //    using var doc = context.OpenAsync(req => req.Content(text)).Result;
+        //    Dictionary<string, string> res;
 
-            var faqs = doc.GetElementsByTagName(FAQ_NAME);
-            if (faqs.Count() == 0) throw new Exception($"Тег {FAQ_NAME} отсутствует в документе");
+        //    var faqs = doc.GetElementsByTagName(FAQ_NAME);
+        //    if (faqs.Count() == 0) throw new Exception($"Тег {FAQ_NAME} отсутствует в документе");
 
-            foreach (var faq in faqs)
-            {
-                res = new Dictionary<string, string>();
+        //    foreach (var faq in faqs)
+        //    {
+        //        res = new Dictionary<string, string>();
 
-                var items = faq.GetElementsByTagName(ITEM_NAME);
-                if (items.Count() == 0) 
-                    throw new Exception($"Тег {ITEM_NAME} отсутствует в документе");
+        //        var items = faq.GetElementsByTagName(ITEM_NAME);
+        //        if (items.Count() == 0) 
+        //            throw new Exception($"Тег {ITEM_NAME} отсутствует в документе");
 
-                IHtmlCollection<IElement> questions, answers;
-                foreach (var item in items)
-                {
-                    questions = item.GetElementsByTagName(QUESTION_NAME);
-                    answers = item.GetElementsByTagName(ANSWER_NAME);
+        //        IHtmlCollection<IElement> questions, answers;
+        //        foreach (var item in items)
+        //        {
+        //            questions = item.GetElementsByTagName(QUESTION_NAME);
+        //            answers = item.GetElementsByTagName(ANSWER_NAME);
 
-                    if (questions.Count() == 0)
-                        throw new Exception($"Тег {QUESTION_NAME} отсутствует в документе");
-                    else if (answers.Count() == 0)
-                        throw new Exception($"Тег {ANSWER_NAME} отсутствует в документе");
+        //            if (questions.Count() == 0)
+        //                throw new Exception($"Тег {QUESTION_NAME} отсутствует в документе");
+        //            else if (answers.Count() == 0)
+        //                throw new Exception($"Тег {ANSWER_NAME} отсутствует в документе");
 
-                    if(res.ContainsKey(questions
-                        .First()
-                        .InnerHtml))
-                        res[questions
-                        .First()
-                        .InnerHtml] = answers
-                        .First()
-                        .InnerHtml;
-                    else res.Add(questions
-                            .First()
-                            .InnerHtml
-                            , answers
-                            .First()
-                            .InnerHtml);
-                }
-                result.Faq.Add(res);
-            }
-            foreach (var element in doc.QuerySelectorAll(FAQ_NAME))
-                element.Replace(element.ChildNodes.ToArray());
-            result.ContentWithoutFaq = doc?.Body?.InnerHtml;
-        }
-        catch (Exception ex)
-        {
-            result.Errors.ErrorsList.Add( new Error(ex.Message) );
-        }
-        
-        return result;
+        //            if(res.ContainsKey(questions
+        //                .First()
+        //                .InnerHtml))
+        //                res[questions
+        //                .First()
+        //                .InnerHtml] = answers
+        //                .First()
+        //                .InnerHtml;
+        //            else res.Add(questions
+        //                    .First()
+        //                    .InnerHtml
+        //                    , answers
+        //                    .First()
+        //                    .InnerHtml);
+        //        }
+        //        result.Faq.Add(res);
+        //    }
+        //    foreach (var element in doc.QuerySelectorAll(FAQ_NAME))
+        //        element.Replace(element.ChildNodes.ToArray());
+        //    result.ContentWithoutFaq = doc?.Body?.InnerHtml;
+        //}
+        //catch (Exception ex)
+        //{
+        //    result.Errors.ErrorsList.Add( new Error(ex.Message) );
+        //}
+        return RegexParse(text);
     }
 }
